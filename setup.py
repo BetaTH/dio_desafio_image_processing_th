62557456d326ae6fb2a619262c9edd40d30c1675
BetaTH <thielson12@gmail.com>
2022-04-10 21:12:21 -0300

    corrigindo um erro em setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="This project is part of a bootcamp",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/BetaTH/image_processing_th.git"
+    url="https://github.com/BetaTH/image_processing_th.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
